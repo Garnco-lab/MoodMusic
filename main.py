3938b51e391a960e0eea979bf44e504e5b089438
Steven <stevendeleonwork@gmaill.com>
2022-03-02 02:55:10 -0800

    voice recognizer added

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ import neuralintents
 from neuralintents import GenericAssistant
 import pyttsx3 as tts
 
+# introduces a recognition software
 recognition = speech_recognition.Recognizer()
 
+# introduces the computer speaker
 computerSpeaker = tts.init()
 computerSpeaker.setProperty('rate', 150)
+
+# The main virtual assistant
+virtualAssistant = GenericAssistant('intents.json')
+# trains model automatically from the library
+virtualAssistant.train_model()
