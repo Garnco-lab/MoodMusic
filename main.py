d9b89274854afaaae89d471f78cf1ab2b01fd089
Steven <stevendeleonwork@gmaill.com>
2022-03-02 03:36:08 -0800

    recognizes voices

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import sys
 import pandas as pd
 import neuralintents
+
 from neuralintents import GenericAssistant
 import pyttsx3 as tts
 
@@ -15,11 +16,36 @@ recognition = speech_recognition.Recognizer()
 computerSpeaker = tts.init()
 computerSpeaker.setProperty('rate', 150)
 
+
+def helloWorld():
+    print("hello world")
+
+def recognize_sad():
+    computerSpeaker.say("Okay lets play something sad")
+
+
+mappings = {
+    'sad': recognize_sad(),
+    'happy': helloWorld,
+    'angry': helloWorld
+}
+
 # The main virtual assistant
-virtualAssistant = GenericAssistant('intents.json')
+virtualAssistant = GenericAssistant('intents.json', intent_methods=mappings)
 # trains model automatically from the library
 virtualAssistant.train_model()
 
-mappings = {
-    # early mapping file progress
-}
+while True:
+
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognition.adjust_for_ambient_noise(mic, duration=0.2)
+            voice_audio = recognition.listen(mic)
+
+            messenger = recognition.recognize_google(voice_audio)
+            messenger = messenger.lower()
+
+        virtualAssistant.request(messenger)
+        # re-instantiate voice
+    except speech_recognition.UnknownValueError:
+        recognition = speech_recognition.Recognizer()
